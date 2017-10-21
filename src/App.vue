08fed7026dbd7a74e51e5710ab5a9e370221b22c
rpadmarao <rohit.padmarao@gmail.com>
2017-10-21 13:57:19 -0500

    map built, still have to add grid and lookup functionality

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBo3L6AEYoMI1ot6dlyS8nVxyEgzQ0nOLc' ></script>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /*
+  margin-top: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  margin-bottom: 0px;
+  padding-top: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  padding-bottom: 0px;
+  */
+}
+</style>
---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,45 @@
+/* global google */
+
+<template>
+  <div class='google-map' :id="mapName"></div>
+</template>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBo3L6AEYoMI1ot6dlyS8nVxyEgzQ0nOLc" ></script>
+<script>
+export default {
+  name: 'google-map',
+  props: ['name'],
+  data: function () {
+    return {
+      mapName: this.name + '-map'
+    }
+  },
+  /*eslint-disable*/
+  mounted: function () {
+    const element = document.getElementById(this.mapName)
+    const options = {
+      zoom: 14,
+      center: new google.maps.LatLng(41.997498, -93.632537)
+    }
+
+    const map = new google.maps.Map(element, options)
+
+    map.data.addListener('click', function(event){
+      
+    });
+  }
+  /*eslint-enable*/
+}
+</script>
+
+<style scoped>
+.google-map {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  margin: 0 auto;
+  background: gray;
+}
+</style>
